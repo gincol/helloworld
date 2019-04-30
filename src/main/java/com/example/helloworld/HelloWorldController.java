0b58d9ea2b7fb0dc2bd4b3728c158bd4f254d53b
gcollado <gcollado@viewnext.com>
2019-04-30 20:01:50 +0200

    first commit

---
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.example.helloworld;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+
+	private static final Logger log = LoggerFactory.getLogger(HelloWorldController.class);
+
+
+	@GetMapping(path = "/world")
+	public @ResponseBody String getAllUsers() {
+		log.info("Busqueda de todos los usuarios");
+		return "Hello World";
+	}
+	
+}
